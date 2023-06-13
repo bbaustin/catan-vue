6c018470330887b2ce6e1c3c6fc40947b1316d4a
bbaustin <benjaminboydaustin@gmail.com>
2023-06-13 22:06:41 +0900

    Change vite.config.js and package.json to deploy to different URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://hereismy.website/catan-dice-roller-2/',
+  base: 'https://catan.hereismy.website/',
 });
